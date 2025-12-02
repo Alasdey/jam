f8b29c3712858a2bb110962ba4829052b8175565
Nofin Really <metaturgie@gmail.com>
2025-12-02 10:55:09 +0000

    restoring subleq only in the placeholder reward, one thing at a time

--- rewards/placeholder_reward.py
+++ rewards/placeholder_reward.py
@@ -4,17 +4,17 @@ from typing import List
 from interpreters.subleq.subleq import subleq
 
 
-def reward(interpreter, code_a: List[int], code_b: List[int], max_output_length: int, max_iterations: int) -> int:
+def reward(code_a: List[int], code_b: List[int], max_output_length: int, max_iterations: int) -> int:
     """
     Reward obtained by playing both side of the subleq game with deterministic reward -1, 0 or 1
     The more the better for A 
     """
     staple = [0, 1, 2, 3, 4, 5]
-    out_a = interpreter(code_a, staple, max_output_length, max_iterations)
-    out_b = interpreter(code_b, staple, max_output_length, max_iterations)
+    out_a = subleq(code_a, staple, max_output_length, max_iterations)
+    out_b = subleq(code_b, staple, max_output_length, max_iterations)
     
     if len(out_a) == len(out_b):
         return 0
     elif len(out_a) > len(out_b):
         return 1
-    return -1
+    return 0
